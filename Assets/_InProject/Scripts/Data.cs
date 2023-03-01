550bde93d60cb82385e4844e4d39b942ef270259
Zekeriyaozden <zekeriyaozden@icloud.com>
2023-03-01 13:47:36 +0300

    DataManager

--- Assets/_InProject/Scripts/Data.cs
+++ Assets/_InProject/Scripts/Data.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Data
+public class QuestionData
 {
+    public int id;
     public string question;
     public List<string> answers;
     public int sizeOfAnswer;
+    public string subject;
 
-    public Data(int _sizeOfAnswer,string _question,List<string> answers)
+    public QuestionData(int _sizeOfAnswer,string _question,List<string> _answers,int _id,string _subject)
     {
-        
+        this.sizeOfAnswer = _sizeOfAnswer;
+        this.answers = _answers;
+        this.id = _id;
+        this.question = _question;
+        this.subject = _subject;
     }
+    
+    
+    
+    
+    
 }
+
--- Assets/_InProject/Scripts/DataReader.cs
+++ Assets/_InProject/Scripts/DataReader.cs
@@ -1,20 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = System.Object;
 
 public class DataReader : MonoBehaviour
 {
-    
+    public TextAsset questdata;
+    public List<QuestionData> dataSet;
     void Start()
     {
-        TextAsset questdata = Resources.Load<TextAsset>("quesdata");
-        string[] data = questdata.text.Split("\n");
-        Debug.Log(data.Length);
+        dataSet = new List<QuestionData>();
+        Read();
     }
-
-    // Update is called once per frame
-    void Update()
+    void Read()
     {
-        
+        string[] Lines = questdata.text.Split("\n");
+        //Debug.Log(Lines.Length);
+        for (int i=0; i<Lines.Length; i++)
+        {
+            //Debug.Log(Lines[i]);
+            string[] dataLine = Lines[i].Split(",");
+            int id, answerSize;
+            List<string> answer = new List<string>();
+            string question , subject;
+            
+            Int32.TryParse(dataLine[0],out id);
+            if (id == 0)
+            {
+                continue;
+            }
+            else
+            {
+                answer.Add(dataLine[2]);
+                answer.Add(dataLine[3]);
+                answer.Add(dataLine[4]);
+                question = dataLine[1];
+                subject = dataLine[7];
+                if (dataLine[5] == "")
+                {
+                    answerSize = 3;
+                }else if (dataLine[6] == "")
+                {
+                    answerSize = 4;
+                    answer.Add(dataLine[5]);
+                }
+                else
+                {
+                    answerSize = 5;
+                    answer.Add(dataLine[5]);
+                    answer.Add(dataLine[6]);
+                }
+                QuestionData dt = new QuestionData(answerSize,question,answer,id,subject);
+                dataSet.Add(dt);
+            }
+            
+        }
+
+        for (int i = 0; i < dataSet.Count; i++)
+        {
+            Debug.Log(dataSet[i].sizeOfAnswer);
+        }
     }
 }
+
